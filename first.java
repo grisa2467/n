8aa185ccd429a3c054e8a24df86c50c05ec98605
Grigore <69109586+grisa2467@users.noreply.github.com>
2022-03-03 16:39:44 +0200

    Create first.java

---
+++ first.java
@@ -0,0 +1,11 @@
+ines (8 sloc)  231 Bytes
+  
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
